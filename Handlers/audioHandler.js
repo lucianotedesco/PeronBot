44161cea3985ad93a53a665d779d23cd679b177b
Luciano Tedesco <ltedesco@entaconsulting.com>
2020-09-14 21:58:51 -0300

    Esperar 2s aantes de dejar el voicechannel luego de audio

--- Handlers/audioHandler.js
+++ Handlers/audioHandler.js
@@ -6,7 +6,7 @@ module.exports = {
         });
 
         dispatcher.on('end', () => {
-            connection.disconnect();
+            setTimeout(connection.disconnect(),2000);
         });
     },         
 };
